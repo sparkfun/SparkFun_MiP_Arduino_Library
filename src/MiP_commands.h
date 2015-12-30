0c6d4314e12df5a4e82e6c784b61ec743ca03c3e
2stacks <shannon.stanley@gmail.com>
2015-12-29 23:33:42 -0500

    Enabled stop function

--- src/MiP_commands.h
+++ src/MiP_commands.h
@@ -26,8 +26,9 @@ class MiP {
 /*    void timeDrive(int8_t direction, int8_t speed, uint8_t time);
     void turnAngle(int8_t direction, int8_t speed, uint8_t angle);
     void continuousDrive(int8_t direction, int8_t speed);
+*/
     void stop(void);
-
+/*
     void setGameMode(int8_t mode);
 
     void getStatus(Parameter parameter); //TODO create struct for status
